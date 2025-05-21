f6080a3552bcd57c2c93521d2e6a40abbbb0d254
Mariane Fatima <fmari6979@gmail.com>
2025-05-21 17:12:43 -0300

    inicio do login

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,9 +1,38 @@
 function Login() {
-    return(
-        <>
+    return (
+      <>
         <h1>login</h1>
+        <div className="body-login">
+          
+            <div className="card-body">
+              <form>
+                <div className="form-group">
+                  <label htmlFor="nickName">Usuário</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="nickName"
+                    placeholder="Usuário"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Senha</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    placeholder="Senha"
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Entrar
+                </button>
+              </form>
+              <p>Não é cadastrado? cadastre-se aqui</p>
+            </div>
+          </div>
         <footer></footer>
-        </>
-    )
+      </>
+    );
 }
 export default Login
