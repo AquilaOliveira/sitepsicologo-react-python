6652494bf73292becac5dcc821c8dd7725ac0f71
Raissa <raissa.feitosa@gmail.com>
2025-05-28 21:17:52 -0300

    Criação da página de acesso

--- frontend/src/pages/acesso.jsx
+++ frontend/src/pages/acesso.jsx
@@ -1,8 +1,59 @@
+import React from "react";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import AgendamentoCard from "../pages/pagesAssets/AgendamentoCard"; 
+import "./Acesso.css";
+
 function Acesso() {
+  const agendamentos = [
+    {
+      data: "18/04/2025",
+      hora: "15:00",
+      nomePaciente: "maisa ferreira",
+      psicologa: "Letícia Dias",
+    },
+    {
+      data: "27/04/2025",
+      hora: "15:00",
+      nomePaciente: "maisa ferreira",
+      psicologa: "Letícia Dias",
+    },
+    
+  ];
+
+  const handleCancelar = (index) => {
+    alert(`Cancelando sessão ${index + 1}...`);
+  };
+
   return (
     <>
-      <h1>acesso</h1>
+      <Header isLogin={true} />
+      <main className="acesso-container">
+        <h1>Próximos agendamentos</h1>
+        <div style={containerStyle}>
+          {agendamentos.map((item, index) => (
+            <AgendamentoCard
+              key={index}
+              data={item.data}
+              hora={item.hora}
+              nomePaciente={item.nomePaciente}
+              psicologa={item.psicologa}
+              onCancelar={() => handleCancelar(index)}
+            />
+          ))}
+        </div>
+      </main>
+      <Footer />
     </>
   );
 }
+
+const containerStyle = {
+  display: "flex",
+  flexWrap: "wrap",
+  justifyContent: "center",
+  gap: "20px",
+  marginTop: "30px"
+};
+
 export default Acesso;
