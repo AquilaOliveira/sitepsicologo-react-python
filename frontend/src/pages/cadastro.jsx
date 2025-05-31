6b4dc90480191eb25f2723818558ef9c64529ed4
Thaina de Souza <souza.thainap@gmail.com>
2025-05-30 22:42:40 -0300

    ajustando switch e seta

--- frontend/src/pages/cadastro.jsx
+++ frontend/src/pages/cadastro.jsx
@@ -3,7 +3,6 @@ import "./Cadastro.css";
 import { useState } from "react";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
-import seta from "../assets/icons/seta.ico";
 
 
 export default function CadastroForm() {
@@ -105,6 +104,8 @@ export default function CadastroForm() {
   regex.test(valor) ? campo.classList.remove("error") : console.log("Senha inválida");
   }
 
+  const seta = <svg viewBox="0 0 15px 15px" width='25px' height='25px' fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fillRule="evenodd" clipRule="evenodd" d="M12.2929 4.29289C12.6834 3.90237 13.3166 3.90237 13.7071 4.29289L20.7071 11.2929C21.0976 11.6834 21.0976 12.3166 20.7071 12.7071L13.7071 19.7071C13.3166 20.0976 12.6834 20.0976 12.2929 19.7071C11.9024 19.3166 11.9024 18.6834 12.2929 18.2929L17.5858 13H4C3.44772 13 3 12.5523 3 12C3 11.4477 3.44772 11 4 11H17.5858L12.2929 5.70711C11.9024 5.31658 11.9024 4.68342 12.2929 4.29289Z" fill="#ffffff"></path> </g></svg>
+
   const successMessage = () => {
     return (
       <div className="success" style={{ display: submitted ? "" : "none" }}>
@@ -122,6 +123,21 @@ export default function CadastroForm() {
       </>
     )
   }
+
+/*const mySwitch = document.getElementById('mySwitch');
+
+mySwitch.addEventListener('change', () => {
+  const isChecked = mySwitch.checked;
+  console.log('Switch está marcado?', isChecked);
+
+  if (isChecked) {
+    // Código para executar quando o switch está ativado
+    console.log('Switch está ativado');
+  } else {
+    // Código para executar quando o switch está desativado
+    console.log('Switch está desativado');
+  }
+});*/
 
   return (
     <>
@@ -191,16 +207,16 @@ export default function CadastroForm() {
                   placeholder="Digite sua senha"
                   type="password"
                 />
-                <input
-                  className="input"
-                  type="checkbox"
-                />
+                <label className="switch" id="mySwitch">
+                  <input type="checkbox" defaultChecked></input>
+                  <span className="slider round"></span>
+                </label>
                 <div className="checkbox">
-                  <input type="checkbox" id="aceitar" name="aceitar" required/>
+                  <input type="checkbox" id="aceitar" name="aceitar" required />
                   <label htmlFor="aceitar">Aceito os termos de uso</label>
                 </div>
                 <button onClick={handleSubmit} className="btn" type="submit">
-                  Cadastrar<img className="seta" src={seta} alt="Seta para a direita"/>
+                  <span className="seta">{seta}</span>Cadastrar
                 </button>
                 <div className="cadastrado">
                   <p>Já tem o cadastro?</p>
